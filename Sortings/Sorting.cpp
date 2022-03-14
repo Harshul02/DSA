dce1c39e3630553277669847dc7f951ed8ead331
Harshul Jain <harshulgangwal02@gmail.com>
2022-03-14 23:39:07 +0530

    Added Selection Sort

--- Sortings/Sorting.cpp
+++ Sortings/Sorting.cpp
@@ -43,12 +43,27 @@ void Insertion(int A[], int n)
     }
 }
 
+void Selection(int A[], int n)
+{
+    int i,j,k;
+    for(i=0; i<n-1; i++)
+    {
+        for(j=k=i; j<n; j++)
+        {
+            if(A[j]<A[k])
+                k=j;
+        }
+        swap(&A[i], &A[k]);
+    }
+}
+
 int main()
 {
     int A[]={3,7,9,10,6,5,12,4,11,2};
     int n=10;
     //Bubble(A,10);
-    Insertion(A,n);
+    //Insertion(A,n);
+    Selection(A,n);
     for(int i=0; i<n; i++)
     {
         cout<<A[i]<<" ";
