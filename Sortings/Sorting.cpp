359651f0bba8be7c2e9c874885241dbe75daa0fa
Harshul Jain <harshulgangwal02@gmail.com>
2022-03-14 23:05:52 +0530

    Added Insertion Sort

--- Sortings/Sorting.cpp
+++ Sortings/Sorting.cpp
@@ -27,11 +27,28 @@ void Bubble(int A[], int n)
     }
 }
 
+void Insertion(int A[], int n)
+{
+    int x,i,j;
+    for(i=1; i<n; i++)
+    {
+        j=i-1;
+        x=A[i];
+        while(j>-1 && A[j]>x)
+        {
+            A[j+1]=A[j];
+            j--;
+        }
+        A[j+1]=x;
+    }
+}
+
 int main()
 {
     int A[]={3,7,9,10,6,5,12,4,11,2};
     int n=10;
-    Bubble(A,10);
+    //Bubble(A,10);
+    Insertion(A,n);
     for(int i=0; i<n; i++)
     {
         cout<<A[i]<<" ";
