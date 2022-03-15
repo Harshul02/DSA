99c4475b5f3872d6225c3f3a3d327cf5b0bfe9b6
Harshul Jain <harshulgangwal02@gmail.com>
2022-03-15 22:31:51 +0530

    Added Iterative Merge Sort

--- Sortings/Sorting.cpp
+++ Sortings/Sorting.cpp
@@ -82,14 +82,56 @@ void QuickSort(int A[], int l, int h)
     }
 }
 
+void Merge(int A[], int l, int mid, int h)
+{
+    int i=l,j=mid+1,k=l;
+    int B[100];
+
+    while(i<=mid && j<=h)
+    {
+        if(A[i]<A[j])
+            B[k++]=A[i++];
+        else
+            B[k++]=A[j++];
+    }
+    for(; i<=mid; i++)
+        B[k++]=A[i];
+    for(; j<=h; j++)
+        B[k++]=A[j];
+
+    for(i=l; i<=h; i++)
+    {
+        A[i]=B[i];
+    }
+}
+
+void IMergeSort(int A[], int n)
+{
+    int p,i,l,mid,h;
+
+    for(p=2; p<=n; p=p*2)
+    {
+        for(int i=0; i+p-1<n; i=i+p)
+        {
+            l=i;
+            h=i+p-1;
+            mid=(l+h)/2;
+            Merge(A,l,mid,h);
+        }
+    }
+    if(p/2<n)
+        Merge(A,0,p/2-1,n-1);
+}
+
 int main()
 {
-    int A[]={3,7,9,10,6,5,12,4,11,2,INT_MAX};
-    int n=11;
+    int A[]={3,7,9,10,6,5,12,4,11,2};
+    int n=10;
     //Bubble(A,10);
     //Insertion(A,n);
     //Selection(A,n);
-    QuickSort(A,0,n-1);
+    //QuickSort(A,0,n-1);
+    IMergeSort(A,n);
     for(int i=0; i<n; i++)
     {
         cout<<A[i]<<" ";
