5529743d5e85338f8d6fdaa68d9f262922865a80
Harshul Jain <harshulgangwal02@gmail.com>
2023-01-26 11:12:51 +0530

    Cheapest Flight With K Stops

---
+++ Graph/Striver Graph Series/Cheapest Flight With K Stops.cpp
@@ -0,0 +1,36 @@
+int CheapestFLight(int n, vector<vector<int>>& flights, int src, int dst, int K)  {
+        // Code here
+        vector<pair<int,int>> adj[n];
+        for(int i=0; i<flights.size(); i++)
+        {
+            adj[flights[i][0]].push_back({flights[i][1],flights[i][2]});
+        }
+        vector<int> dist(n, 1e9);
+        queue<pair<int,pair<int,int>>> q;
+        dist[src] = 0;
+        q.push({0,{src,0}});
+        while(!q.empty())
+        {
+            auto it = q.front();
+            q.pop();
+            int stop = it.first;
+            int node = it.second.first;
+            int cost = it.second.second;
+            
+            if(stop > K) continue;
+            
+            for(auto it: adj[node])
+            {
+                int adjNode = it.first;
+                int edW = it.second;
+                
+                if(cost + edW < dist[adjNode] and stop<=K)
+                {
+                    dist[adjNode] = cost + edW;
+                    q.push({stop+1, {adjNode, cost + edW}});
+                }
+            }
+        }
+        if(dist[dst] == 1e9) return -1;
+        return dist[dst];
+    }
