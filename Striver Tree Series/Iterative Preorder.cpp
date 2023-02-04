5eada952ce8cc0ead47ab523af049fb169ddeea6
Harshul Jain <harshulgangwal02@gmail.com>
2023-02-04 19:06:27 +0530

    Iterative Preorder

---
+++ Striver Tree Series/Iterative Preorder.cpp
@@ -0,0 +1,21 @@
+vector<int> preOrder(Node* root)
+    {
+        //code here
+        vector<int> ans;
+        if(!root)
+            return ans;
+        
+        stack<Node*> st;
+        st.push(root);
+        while(!st.empty())
+        {
+            Node* temp = st.top();
+            st.pop();
+            ans.push_back(temp->data);
+            if(temp->right)
+                st.push(temp->right);
+            if(temp->left)
+                st.push(temp->left);
+        }
+        return ans;
+    }
