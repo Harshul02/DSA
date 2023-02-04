7282fb21e90e3c5069165bb33bffd6041f23f7ea
Harshul Jain <harshulgangwal02@gmail.com>
2023-02-04 18:49:10 +0530

    Inorder Traversal

---
+++ Striver Tree Series/Inorder Traversal.cpp
@@ -0,0 +1,16 @@
+void inorder(Node *root, vector<int> &ans)
+    {
+        if(!root)
+            return;
+            
+        inorder(root->left, ans);
+        ans.push_back(root->data);
+        inorder(root->right, ans);
+    }
+    
+    vector<int> inOrder(Node* root) {
+        // Your code here
+        vector<int> ans;
+        inorder(root, ans);
+        return ans;
+    }
