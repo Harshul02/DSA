c81ac9a97167733d445ebf692b1bf1421cc13939
Harshul Jain <harshulgangwal02@gmail.com>
2023-02-23 23:18:53 +0530

    Optimized Code

--- Practice Questions/Priority Queue Sheet/Kth smallest element in a sorted matrix.cpp
+++ Practice Questions/Priority Queue Sheet/Kth smallest element in a sorted matrix.cpp
@@ -12,3 +12,23 @@ int kthSmallest(vector<vector<int>>& matrix, int k) {
         }
         return pq.top();
     }
+
+//--------------------------------------------------------------------------------------------------
+// Optimized Approach
+
+        int n = matrix.size();
+        priority_queue<pair<int, pair<int,int>>, vector<pair<int,pair<int,int>>>, greater<>> pq;
+        for(int i=0; i<n; i++)
+        {
+            pq.push({matrix[i][0], {i,0}});
+        }
+        int ans = 0;
+        while(!pq.empty() and k--)
+        {
+            int x = pq.top().second.first, y = pq.top().second.second;
+            ans = pq.top().first;
+            pq.pop();
+            if(y != n-1)
+                pq.push({matrix[x][y+1], {x, y+1}});
+        }
+        return ans;
