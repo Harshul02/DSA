4248477dd3174b8c0403751bdbdb4c88592675e8
Harshul Jain <harshulgangwal02@gmail.com>
2023-02-26 23:53:54 +0530

    Split Array Into Subsequences

---
+++ Practice Questions/Priority Queue Sheet/Split array into consecutive subsequences.cpp
@@ -0,0 +1,27 @@
+bool isPossible(vector<int>& nums) {
+        map<int,int> mp, mp1;
+
+        for(auto it : nums){
+            mp[it]++;
+        }
+        for(auto it : nums)
+        {
+            if(mp[it] == 0)
+                continue;
+            mp[it]--;
+
+            if(mp1[it-1] > 0)
+            {
+                mp1[it-1]--;
+                mp1[it]++;
+            }
+            else if(mp[it+1] and mp[it+2])
+            {
+                mp[it+1]--;
+                mp[it+2]--;
+                mp1[it+2]++;
+            }
+            else return false;
+        }
+        return true;
+    }
