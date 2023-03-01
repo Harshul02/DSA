4bb696ece61a1760f2eb2363a0f25cb8ed7217dc
Harshul Jain <harshulgangwal02@gmail.com>
2023-03-01 16:44:44 +0530

    Partition labels

---
+++ Practice Questions/String/Partition Labels.cpp
@@ -0,0 +1,22 @@
+vector<int> partitionLabels(string s) {
+        vector<int> ans;
+
+        unordered_map<char,int> mp;
+        for(int i=0; i<s.length(); i++)
+        {
+            mp[s[i]] = i;
+        }
+
+        int prev = -1;
+        int maxi = 0;
+        for(int i=0; i<s.size(); i++)
+        {
+            maxi = max(maxi, mp[s[i]]);
+            if(maxi == i)
+            {
+                ans.push_back(maxi - prev);
+                prev = maxi;
+            }
+        }
+        return ans;
+    }
