a72062ee592ecc143393b3f23bf148cbe73582a6
Harshul Jain <harshulgangwal02@gmail.com>
2023-03-08 15:44:55 +0530

    Custom sort string

---
+++ Practice Questions/String/Custom sort string.cpp
@@ -0,0 +1,6 @@
+string customSortString(string order, string s) {
+        sort(s.begin(), s.end(), [&](char a, char b){
+            return order.find(a) < order.find(b);
+        });
+        return s;
+    }
