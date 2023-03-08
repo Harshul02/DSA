5fd03773a193987da2ef76d18939bb8a365b6243
Harshul Jain <harshulgangwal02@gmail.com>
2023-03-08 14:59:01 +0530

    Complex Number Multiplication

---
+++ Practice Questions/String/Complex Number Multiplication.cpp
@@ -0,0 +1,9 @@
+string complexNumberMultiply(string num1, string num2) {
+        int ra,ia,rb,ib;
+        char buff;
+        stringstream aa(num1), bb(num2), ans;
+        aa>>ra>>buff>>ia>>buff;
+        bb>>rb>>buff>>ib>>buff;
+        ans<<ra*rb - ia*ib<<"+"<<ra*ib + rb*ia<<"i";
+        return ans.str();
+    }
